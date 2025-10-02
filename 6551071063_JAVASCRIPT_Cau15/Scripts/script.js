29b7c413e7d4324e6cc5ede1b3ccf5f6a0dd7ae3
CNTT6551071063HinhVinhPhat <6551071063@st.utc2.edu.vn>
2025-10-02 19:02:38 +0700

    Add files via upload

---
+++ 6551071063_JAVASCRIPT_Cau15/Scripts/script.js
@@ -0,0 +1,8 @@
+var u={
+    Name:"Max",
+    Age : 20,
+}
+alert("User u info:\n"+"Name: "+u.Name+"\n"+"Age: "+u.Age+"\n");
+u.Surname="Lee";
+u.Age=30;
+console.log("User u info:\n"+"Name: "+u.Name+"\n"+"Surname: "+u.Surname+"\n"+"Age: "+u.Age+"\n")
